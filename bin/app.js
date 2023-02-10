7c61576799e91f49643c28c4f0f1d6baa708dcdf
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-09 16:27:57 -0800

    feat: make module action

--- bin/app.js
+++ bin/app.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const { newApplicationAction } = require('../src/actions');
+const { newApplicationAction, makeModuleAction } = require('../src/actions');
 
 program
   .command('new')
   .description('New Application')
   .argument('<name>', 'Directory Name')
   .action(newApplicationAction);
+
+program
+  .command('make:module')
+  .description('New Module')
+  .argument('<name>', 'Module Name')
+  .action(makeModuleAction);
 
 program.parse();
---
+++ src/actions/make-module.action.js
@@ -0,0 +1,5 @@
+const { print } = require('../../lib/print');
+
+module.exports = async function makeModule(name) {
+  print('Creating module...');
+};
