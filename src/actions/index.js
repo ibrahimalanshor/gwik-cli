abba10ff4df3f0f9f58b9692eb290edf4b8936a9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-11 06:07:43 -0800

    feat: make request action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,3 @@
 exports.newApplicationAction = require('./new-application.action');
 exports.makeModuleAction = require('./make-module.action');
+exports.makeRequestAction = require('./make-request.action');
