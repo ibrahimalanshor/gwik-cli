6e6220bace9c7b980f4c832e8c854c32ae188748
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-11 20:42:11 -0800

    feat: generate controller crud

--- src/templates/generate-controller.js
+++ src/templates/generate-controller.js
@@ -1,15 +1,40 @@
 const { toCase, tokenReplace } = require('../../lib/string');
 const { pluralize } = require('../../lib/string');
 
-module.exports = function generateController(name) {
+function baseController(name, options = {}) {
+  const action = options.action || 'get';
+  const returnData = options.return || '[]';
+
   const template = `exports.{function} = function {function}() {
   return {return};
 };`;
 
   const data = {
-    '{function}': toCase.toCamelCase('get', ...pluralize(name).split(' ')),
-    '{return}': `[]`,
+    '{function}': toCase.toCamelCase(action, ...pluralize(name).split(' ')),
+    '{return}': returnData,
   };
 
   return tokenReplace(template, data);
+}
+
+function crudController(name) {
+  const actions = {
+    get: '[]',
+    create: '{}',
+    find: '{}',
+    update: '{}',
+    delete: '{}',
+  };
+
+  return Object.entries(actions).reduce((res, [action, returnData]) => {
+    return (res += baseController(name, { action, return: returnData }) + '\n');
+  }, '');
+}
+
+module.exports = function generateController(name, options = {}) {
+  if (options.type === 'crud') {
+    return crudController(name);
+  }
+
+  return baseController(name, options);
 };
