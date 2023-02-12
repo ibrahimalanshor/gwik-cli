35b7cf8e0c37e928f1744d8527e67813ca797426
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-11 16:09:45 -0800

    feat: make middleware action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 exports.newApplicationAction = require('./new-application.action');
 exports.makeModuleAction = require('./make-module.action');
 exports.makeRequestAction = require('./make-request.action');
+exports.makeMiddlewareAction = require('./make-middleware.action');
