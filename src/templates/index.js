ba0f03995efd96ca456b645ac8fbdf3287030f10
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-11 16:09:30 -0800

    feat: generate middleware template

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,3 +1,4 @@
 exports.generateController = require('./generate-controller');
 exports.generateRouter = require('./generate-router');
 exports.generateRequest = require('./generate-request');
+exports.generateMiddleware = require('./generate-middleware');
